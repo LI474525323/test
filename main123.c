69ffb1e04f03de0704d87de31086042782e6f2d2
LI474525323 <474525323@qq.com>
2020-02-02 18:10:40 +0800

    -m2020/2/2

---
+++ main123.c
@@ -0,0 +1,44 @@
+#include "led.h"
+#include "delay.h"
+#include "key.h"
+#include "sys.h"
+#include "usart.h"
+#include "timer.h"
+ 
+/************************************************
+ ALIENTEK战舰STM32开发板实验9
+ PWM输出实验  
+ 技术支持：www.openedv.com
+ 淘宝店铺：http://eboard.taobao.com 
+ 关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+ 广州市星翼电子科技有限公司  
+ 作者：正点原子 @ALIENTEK
+************************************************/
+
+	
+ int main(void)
+ {		
+ 	u16 led0pwmval=0;
+	u8 dir=1;	
+	delay_init();	    	 //延时函数初始化	  
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); 	 //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
+	uart_init(115200);	 //串口初始化为115200
+ 	LED_Init();			     //LED端口初始化
+ 	TIM3_PWM_Init(999,44);	 //不分频。PWM频率=72000000/900=80Khz
+   	while(1)
+	{
+ 		delay_ms(10);  
+		GPIO_SetBits(GPIOC,GPIO_Pin_8);	
+ 		TIM_SetCompare2(TIM3,200);
+		delay_ms(500);
+		TIM_SetCompare2(TIM3,0);
+		delay_ms(500);
+		GPIO_ResetBits(GPIOC,GPIO_Pin_8);	
+ 		TIM_SetCompare2(TIM3,200);
+		delay_ms(500);	
+		TIM_SetCompare2(TIM3,0);
+		delay_ms(500);
+	
+	}	 
+ }
+
